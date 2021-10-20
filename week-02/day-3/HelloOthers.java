d97c4dd90608910186e7de5e72fe2c76bfee900d
herszi <szilvia.herczog@yahoo.com>
2021-10-20 15:42:01 +0200

    add exercise HelloOthers

---
+++ week-02/day-3/HelloOthers.java
@@ -0,0 +1,14 @@
+package printonconsole;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Üdvözöld 3 osztálytársad 3 sorban kiiratva a nevüket, ehhez hasonlóan:
+        //
+        // Helló Eszter!
+        // Helló Maris!
+        // Helló Józsi!
+        System.out.println("Hello Eszter!");
+        System.out.println("Hello Maris!");
+        System.out.println("Hello Józsi!");
+    }
+}
