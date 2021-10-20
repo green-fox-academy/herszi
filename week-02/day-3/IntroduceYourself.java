7abc9d2ccd710bd3eb877322bbd59ca66d66a75e
herszi <szilvia.herczog@yahoo.com>
2021-10-20 16:56:28 +0200

    add exercise IntroduceYourself

---
+++ week-02/day-3/IntroduceYourself.java
@@ -0,0 +1,20 @@
+package typesofvariables;
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Írj egy programot, ami kiír rólad pár részletet a konzolra
+        // Minden egyes részletet új sorba írja a program:
+        //  - Neved
+        //  - Korod
+        //  - Magasságod méterben kifejezve (tört érték)
+        //
+        //  Példa kimenet (output):
+        //  John Doe
+        //  31
+        //  1.87
+        
+        System.out.println("Szilvia");
+        System.out.println(37);
+        System.out.println(1.66);
+    }
+}
