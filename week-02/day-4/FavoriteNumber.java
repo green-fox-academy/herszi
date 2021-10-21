fcfb0125f5d07cf180d8e8b18358f35d447e674a
herszi <szilvia.herczog@yahoo.com>
2021-10-21 21:53:45 +0200

    add exercise FavoriteNumber

---
+++ week-02/day-4/FavoriteNumber.java
@@ -0,0 +1,14 @@
+package valiables;
+
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        int favoriteNumber = 8;
+
+        // Írd át a fenti számot a kedvenc számoddá
+        // És utána írasd ki efféle képpen: "A kedvenc számom: 8"
+        // Kiíratáskor használd fel a favoriteNumber változót
+
+        favoriteNumber = 25;
+        System.out.println("A kedvenc számom: " + favoriteNumber);
+    }
+}
