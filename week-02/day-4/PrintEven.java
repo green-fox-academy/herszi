d901d396cc73392079a7338b1bb940061f5b81ac
herszi <szilvia.herczog@yahoo.com>
2021-10-21 21:51:07 +0200

    add exercise PrintEven

---
+++ week-02/day-4/PrintEven.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class PrintEven {
+    public static void main(String[] args) {
+        // Írj egy programot, ami kiírja az összes páros számot 0 és 500 között
+
+        for (int i = 0; i <= 500; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
+
