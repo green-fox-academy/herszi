5e9bd1d23307e4563377e2252da36fbffecfc005
herszi <szilvia.herczog@yahoo.com>
2021-10-23 19:55:30 +0200

    add exercie SumElements

---
+++ week-02/day-6/SumElements.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class SumElements {
+    public static void main(String[] args) {
+        int [] numbers = {54, 23, 66, 12};
+        int sum = numbers[1] + numbers[2];
+        System.out.println("A tömb második és harmadik eleménel az összege: " + sum);
+    }
+}
+
+// - Készíts egy `numbers` nevű változót (tömböt)
+//   a következő tartalommal (értékekkel): `[54, 23, 66, 12]`
+// - Írasd ki a konzolra a `numbers` nevű tömb második és harmadik elemének összegét
