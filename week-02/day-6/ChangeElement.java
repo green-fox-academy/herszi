187da98c54738c22fe5b334f8639d35cc198f8e6
herszi <szilvia.herczog@yahoo.com>
2021-10-23 19:52:25 +0200

    add exercise ChangeElement

---
+++ week-02/day-6/ChangeElement.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class ChangeElement {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 8, 5, 6};
+        numbers[3] = 4;
+        System.out.println("A tömb negyedik eleme: " + numbers[3]);
+
+    }
+}
+
+// - Készíts egy `numbers` nevű változót (tömböt)
+//   a következő tartalommal (értékekkel): `[1, 2, 3, 8, 5, 6]`
+// - Változtasd meg a 8-ast 4-esre
+// - Írasd ki a konzolra a `numbers` nevű tömb negyedik elemét
