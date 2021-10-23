360502d01264682dcdab7391eb3c5860c60d81f1
herszi <szilvia.herczog@yahoo.com>
2021-10-23 19:53:50 +0200

    add exercie DoubleItems

---
+++ week-02/day-6/DoubleItems.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class DoubleItems {
+    public static void main(String[] args) {
+        int[] numList = {3, 4, 5, 6, 7};
+
+        for (int i = 0; i < numList.length; i++) {
+            System.out.print((numList[i] = numList[i] * 2) + " ");
+        }
+    }
+}
+
+// - Készíts egy `numList` nevű változót (tömböt)
+//   a következő tartalommal (értékekkel): `[3, 4, 5, 6, 7]`
+// - Duplázd meg minden elem értéket a tömbben
