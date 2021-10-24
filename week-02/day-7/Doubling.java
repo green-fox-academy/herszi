241e3aec1f98269137d1d615cf8e04c13149b744
herszi <szilvia.herczog@yahoo.com>
2021-10-24 21:00:56 +0200

    add exercise Doubling

---
+++ week-02/day-7/Doubling.java
@@ -0,0 +1,20 @@
+package funcions;
+
+public class Doubling {
+    public static void main(String[] args) {
+        int baseNum = 123;
+        System.out.println(doubling(baseNum));
+    }
+
+    public static int doubling(int baseNum) {
+
+        return 2 * baseNum;
+    }
+}
+// - Hozz létre egy `baseNum` nevű integer változót, aminek az értéke legyen `123`.
+
+// - Készíts egy függvényt `doubling` névvel, mely megduplázza a függvény
+//   bemeneti paraméterét és integer-ként adja vissza(return).
+
+// - Írasd ki a visszatérési értékét a `doubling(baseNum)` függvénynek.
+
